213dc9d2de392f08acc886b072bce8463d409180
Fuhai Li <55822442+mslfh@users.noreply.github.com>
2025-02-28 16:01:47 +1100

    Update the landing page to include an avatar sample

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,36 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
+// Quasar
+import { Quasar } from 'quasar'
+import quasarLang from 'quasar/lang/zh-CN'
+import quasarLangZhCN from 'quasar/lang/zh-CN';
+import quasarLangEnUS from 'quasar/lang/en-US';
+
+// Import icon libraries
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/material-symbols-outlined/material-symbols-outlined.css'
+import '@quasar/extras/material-symbols-rounded/material-symbols-rounded.css'
+import '@quasar/extras/material-symbols-sharp/material-symbols-sharp.css'
+import '@quasar/extras/mdi-v6/mdi-v6.css'
+import '@quasar/extras/fontawesome-v6/fontawesome-v6.css'
+import '@quasar/extras/eva-icons/eva-icons.css'
+import '@quasar/extras/themify/themify.css'
+import '@quasar/extras/line-awesome/line-awesome.css'
+import '@quasar/extras/bootstrap-icons/bootstrap-icons.css'
+
+// A few examples for animations from Animate.css:
+// import @quasar/extras/animate/fadeIn.css
+// import @quasar/extras/animate/fadeOut.css
+
+// Import Quasar css
+
+import 'quasar/dist/quasar.css'
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
@@ -15,6 +45,12 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(Quasar, {
+                lang: quasarLangZhCN,
+                plugins: {
+
+                }, // import Quasar plugins and add here
+              })
             .mount(el);
     },
     progress: {
--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -5,6 +5,34 @@ import createServer from '@inertiajs/vue3/server';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
+
+// Quasar
+import { Quasar } from 'quasar'
+import quasarLang from 'quasar/lang/zh-CN'
+
+// Import icon libraries
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/material-symbols-outlined/material-symbols-outlined.css'
+import '@quasar/extras/material-symbols-rounded/material-symbols-rounded.css'
+import '@quasar/extras/material-symbols-sharp/material-symbols-sharp.css'
+import '@quasar/extras/mdi-v6/mdi-v6.css'
+import '@quasar/extras/fontawesome-v6/fontawesome-v6.css'
+import '@quasar/extras/eva-icons/eva-icons.css'
+import '@quasar/extras/themify/themify.css'
+import '@quasar/extras/line-awesome/line-awesome.css'
+import '@quasar/extras/bootstrap-icons/bootstrap-icons.css'
+
+// A few examples for animations from Animate.css:
+// import @quasar/extras/animate/fadeIn.css
+// import @quasar/extras/animate/fadeOut.css
+
+// Import Quasar css
+import 'quasar/dist/quasar.css'
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createServer((page) =>
@@ -16,10 +44,14 @@ createServer((page) =>
         setup({ App, props, plugin }) {
             return createSSRApp({ render: () => h(App, props) })
                 .use(plugin)
+                .use(Quasar, {
+                    plugins: {}, // import Quasar plugins and add here
+                  })
                 .use(ZiggyVue, {
                     ...page.props.ziggy,
                     location: new URL(page.props.ziggy.location),
                 });
+
         },
     })
 );
